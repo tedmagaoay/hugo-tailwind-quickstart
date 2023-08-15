a9470a3446bc4dccc57884721da1cb9c626f03be
tedcm <33870216+tedcm@users.noreply.github.com>
2023-08-15 15:48:22 +0800

    integrate tailwind css

--- themes/my-theme/tailwind.config.js
+++ themes/my-theme/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["content/**/*.md", "layouts/**/*.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
